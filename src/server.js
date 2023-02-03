a3d1914c662c6fc20c477cc7e1f6a26b48047d30
Federico <federicolucaspontoriero@gmail.com>
2023-02-03 14:23:45 -0300

    added server

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import "./config";
+import Database from "./database";
+import environment from "./config/environment";
+import dbConfig from "./config/database";
+
+(async () => {
+  try {
+    const db = new Database(environment.nodeEnv, dbConfig);
+    await db.connect();
+  } catch (err) {
+    console.log(err);
+  }
+})();
